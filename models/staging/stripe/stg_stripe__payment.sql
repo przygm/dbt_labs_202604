60c50142cd46a30c5f8f7f66c478e1ae7cc8ef46
Przemysław Zygmunt <p.zygmunt@interia.pl>
2026-04-10 13:59:58 +0000

    added singular test for stg_stripe__payment

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -29,6 +29,7 @@ select
     paymentmethod as payment_method,
     status as payment_status,
     -- amount is stored in cents, convert it to dollars
+    amount,
     amount / 100 as payment_amount,
     created as payment_created
 from {{ source('stripe', 'payment') }}
